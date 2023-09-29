bd71e2b19ec5880e11b96ae96829d40c16cb7653
gauravbarakoti <bestgaurav1234@gmail.com>
2023-09-29 14:03:34 +0530

    chagnes regarding docker specfic container try 5

--- Flask_application.py
+++ Flask_application.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_flask_application():
-    return 'Welcome to first Flask application, Hi all from runner circleci.........testing this.....removing specific container'
+    return 'Welcome to first Flask application, Hi all from runner circleci.........testing this.....removing specific container........changing configuration deploy new ______________'
 
