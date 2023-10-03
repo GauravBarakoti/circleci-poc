02edb27e918cb5ebca924dd1083095ceb9fca30d
gauravbarakoti <bestgaurav1234@gmail.com>
2023-10-03 17:07:47 +0530

    testing for no downtime testing after applying sleep for each container part 6

--- Flask_application.py
+++ Flask_application.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_flask_application():
-    return 'testing for zero downtime 50 attempt'
+    return '========================================================================='
 
