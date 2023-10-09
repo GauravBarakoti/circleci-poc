3597a189835f47adde2dd83ee02e0b4687d85d2d
gauravbarakoti <bestgaurav1234@gmail.com>
2023-10-09 15:02:39 +0530

    slack new updates

--- Flask_application.py
+++ Flask_application.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_flask_application():
-    return 'Application Up and Running with no downtime,smmooothhh Yay!!'
+    return 'Application Up and Running with no downtime,  smmooothhh  Yay!!'
 
